a5f5fe9f52ee0f1d0cbd2e227857a096bbb0be30
Ashutosh Kapoor <ashutoshkapor1234@gmail.com>
2021-06-27 16:18:28 +0530

    Added cases breadcrumb

--- src/components/layout/NavWrapper/index.js
+++ src/components/layout/NavWrapper/index.js
@@ -1,11 +1,13 @@
 import classes from "./NavWrapper.module.css";
 import { Fragment } from "react";
 import MainNavigation from "../MainNavigation";
+import BreadCrumb from "../../ui/BreadCrumb";
 
 const NavWrapper = (props) => {
   return (
     <Fragment>
       <MainNavigation />
+      <BreadCrumb />
       <main className={classes.main}>{props.children}</main>
     </Fragment>
   );
