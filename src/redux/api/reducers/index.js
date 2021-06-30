2e6b84ffdd4e369b422ead9f8934b7c8cd9efb74
Ashutosh Kapoor <ashutoshkapor1234@gmail.com>
2021-06-30 21:00:58 +0530

    combined api reducers

--- src/redux/api/reducers/index.js
+++ src/redux/api/reducers/index.js
@@ -1,7 +1,16 @@
 import { combineReducers } from "redux";
 import { usersReducer } from "./user.reducer";
 import { productsReducer } from "./product.reducer";
+import { contactsReducer } from "./contact.reducer";
+import { casesReducer } from "./case.reducer";
+import { opportunitiesReducer } from "./opportunity.reducer";
 
 export const apiReducer = {
-  crm: combineReducers({ Users: usersReducer, Products: productsReducer }),
+  crm: combineReducers({
+    Users: usersReducer,
+    Products: productsReducer,
+    Contacts: contactsReducer,
+    Cases: casesReducer,
+    Opportunities: opportunitiesReducer,
+  }),
 };
